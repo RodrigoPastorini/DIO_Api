ca15437dc28d4667b28859adfa567ed92a213464
rodrigopastorfelis <rodrigopastorfelis@gmail.com>
2021-08-30 20:32:27 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIO_Curso.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=RODRIGOPASTORIN\SQLEXPRESS;Initial Catalog=DIO;Integrated Security=True");
+        }
+    }
+}
